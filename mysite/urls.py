a00feea8197e034f7b5ab8540ab0349126053796
quangtv <p-quangtv@nissho-vn.com>
2022-11-21 11:06:10 +0700

    create the public user interface view

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('polls.urls'), name="home_page"),
+    path('polls/', include('polls.urls'), name="home_page"),
 ]
