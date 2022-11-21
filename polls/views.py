edfa37a6caaaec5f0becb15e6bb15f8989fa508c
quangtv <p-quangtv@nissho-vn.com>
2022-11-21 12:22:38 +0700

    add detail view, and solution to store data to DB with clear duplicate value in save

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -32,8 +34,38 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You are looking at the result of question {question_id}!")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    # return HttpResponse(f"You are looking at the result of question {question_id}!")
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You are voting on question {question_id}!")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # FIRST SOLUTION
+        # selected_choice.votes += 1
+        # SECOND SOLUTION
+
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # F() assignments persist after Model.save()
+        # This persistence can be avoided by reloading the model object after saving it,
+        # for example, by using refresh_from_db().
+        selected_choice.refresh_from_db()
+        # selected_choice.save()
+
+        # THIRD SOLUTION
+        # selected_choice.update(stories_filed=F('votes') + 1)
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
